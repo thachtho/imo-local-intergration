c1aad94f60cda8ae83ddb7565d3b189d4b31cb59
thachtho <buithanhtho15ig@gmail.com>
2024-08-31 14:53:11 +0700

    init db

--- clone_repos.sh
+++ clone_repos.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Lui ra một thư mục so với vị trí của file .sh
+cd ..
+
+# Clone các repository
 git clone https://github.com/thachtho/user-service.git
 git clone https://github.com/thachtho/auth-service.git
 git clone https://github.com/thachtho/imo-bff.git
